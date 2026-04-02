76880dffbef13f743ddc1f6abac79f8d8be83c1e
Arizel79 <arizel79@gmail.com>
2026-04-02 20:34:48 +0300

    refactor: edit web server structure

--- src/web/web_server.py
+++ src/web/web_server.py
@@ -5,13 +5,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return send_from_directory(".", "index.html")
+    return send_from_directory("www", "game.html")
 
 
 @app.route("/<path:filename>")
 def static_files(filename):
-    return send_from_directory(".", filename)
+    return send_from_directory("www", filename)
 
+
+def main():
+    app.run(host="0.0.0.0", port=8080)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    main()
