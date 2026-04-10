b9e84bb802b387d7118c413056677d7271a9122b
Arizel79 <arizel79@gmail.com>
2026-04-10 12:47:10 +0300

    some changes

--- src/server/mixins/utils_mixin.py
+++ src/server/mixins/utils_mixin.py
@@ -8,28 +8,6 @@ from src.server.modules.dataclasses import *
 
 
 class UtilsMixin(BaseMixin):
-    def setup_logger(self, name, level=logging.INFO, log_file=None):
-        """Настройка логгера с выводом в консоль и файл."""
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(level)
-
-        file_formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-        )
-        console_formatter = logging.Formatter("[%(levelname)s] %(message)s")
-
-        if not log_file is None:
-            file_handler = logging.FileHandler(log_file, encoding="utf-8")
-            file_handler.setFormatter(file_formatter)
-            self.logger.addHandler(file_handler)
-
-        console_handler = logging.StreamHandler(sys.stdout)
-        console_handler.setFormatter(console_formatter)
-
-        self.logger.addHandler(console_handler)
-
-        return self.logger
-
     def get_color_for_segment(self, snake, segment_n):
         n = segment_n
         color = snake.color
